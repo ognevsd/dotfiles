f0d5b655df7925c9bda31dc08154d9996022cfb3
Sergey Ognev <ognev.s.d@gmail.com>
2025-01-06 19:33:32 +0200

    Add nvim config

---
+++ dot-config/nvim/lua/ognev/plugins/debug.lua
@@ -0,0 +1,73 @@
+return {
+	"mfussenegger/nvim-dap",
+	event = "VeryLazy",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"leoluz/nvim-dap-go",
+		"nvim-neotest/nvim-nio",
+		"mfussenegger/nvim-dap-python",
+	},
+	config = function()
+		local dap = require("dap")
+		vim.keymap.set("n", "<Leader>dt", function()
+			dap.toggle_breakpoint()
+		end)
+		vim.keymap.set("n", "<leader>dc", function()
+			dap.continue()
+		end)
+		vim.keymap.set("n", "<leader>dq", "<Cmd>lua require('dap').terminate()<CR>")
+
+		dap.configurations.go = {
+			{
+				type = "go",
+				name = "Attach to port",
+				mode = "remote",
+				request = "attach",
+				host = "127.0.0.1",
+				port = function()
+					local input = vim.fn.input("Enter the port number for delve:")
+					local port = tonumber(input)
+					if port then
+						return port
+					else
+						print("Invalid port number. Using 12345 instead")
+						return
+					end
+				end,
+			},
+		}
+
+		require("dap-go").setup()
+		local findDebugPath = function()
+			local venvPath = os.getenv("HOME") .. "/.virtualenvs/debugpy/bin/python"
+			local ok, err, code = os.rename(venvPath, venvPath)
+			if not ok then
+				if code == 13 then
+					-- Permission denied, but exists
+					return venvPath
+				end
+				print(err)
+				return "python"
+			end
+			return venvPath
+		end
+		local debugPath = findDebugPath()
+		require("dap-python").setup(debugPath)
+
+		-- DAP UI config
+		local dapui = require("dapui")
+		dapui.setup()
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+	end,
+}
---
+++ dot-config/nvim/lua/ognev/plugins/render-markdown.lua
@@ -0,0 +1,8 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	version = "7.7.0",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {},
+}
