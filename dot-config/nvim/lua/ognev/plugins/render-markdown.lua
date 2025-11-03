d948f42af96580c54997b562532fd52347c0f474
Sergey Ognev <ognev.s.d@gmail.com>
2025-11-03 13:22:45 +0200

    Add latex to render markdown

--- dot-config/nvim/lua/ognev/plugins/render-markdown.lua
+++ dot-config/nvim/lua/ognev/plugins/render-markdown.lua
@@ -1,8 +1,29 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	version = "7.7.0",
+	version = "8.9.0",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
-	opts = {},
+	opts = {
+		latex = {
+			-- Turn on / off latex rendering.
+			enabled = true,
+			-- Additional modes to render latex.
+			render_modes = false,
+			-- Executable used to convert latex formula to rendered unicode.
+			-- If a list is provided the first command available on the system is used.
+			converter = { "utftex", "latex2text" },
+			-- Highlight for latex blocks.
+			highlight = "RenderMarkdownMath",
+			-- Determines where latex formula is rendered relative to block.
+			-- | above  | above latex block                               |
+			-- | below  | below latex block                               |
+			-- | center | centered with latex block (must be single line) |
+			position = "center",
+			-- Number of empty lines above latex blocks.
+			top_pad = 0,
+			-- Number of empty lines below latex blocks.
+			bottom_pad = 0,
+		},
+	},
 }
