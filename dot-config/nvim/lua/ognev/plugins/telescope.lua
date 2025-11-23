d8e549719c028b36d6f26946473574ebe43fe107
Sergey Ognev <ognev.s.d@gmail.com>
2025-11-23 19:07:17 +0200

    Update telescope version

--- dot-config/nvim/lua/ognev/plugins/telescope.lua
+++ dot-config/nvim/lua/ognev/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	event = "VimEnter",
-	tag = "0.1.8",
+	version = "0.1.9",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
